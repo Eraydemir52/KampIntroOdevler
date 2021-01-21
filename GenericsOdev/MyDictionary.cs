1785118ee4ff96653393b8765825008cf6005f38
Eray Demir <2200656036@nku.edu.tr>
2021-01-21 14:58:44 +0300

    21 Ocak 4. DersOdevi (Engin Demiroğ)

---
+++ GenericsOdev/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsOdev
+{
+    class MyDictionary<T>
+    {
+        T[] array;
+
+        public MyDictionary()
+        {
+            array = new T[0];
+        }
+
+
+        public void Add(T item)
+        {
+            T[] temparray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                temparray[i] = array[i];
+            }
+
+            array[array.Length - 1] = item;
+        }
+
+        public int Count
+       {
+            get {return array.Length; }
+       }
+       
+
+    }
+}
---
+++ GenericsOdev/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> myDictionary = new MyDictionary<int>();
+            myDictionary.Add(3);
+            myDictionary.Add(4);
+            myDictionary.Add(5);
+            myDictionary.Add(6);
+            myDictionary.Add(7);
+            Console.WriteLine(myDictionary.Count);
+          
+        }
+    }
+}
