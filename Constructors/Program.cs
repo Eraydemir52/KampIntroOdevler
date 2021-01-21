6a264a0dc4b448e98cb5f319e597b2ebf77fa2e9
Eray Demir <2200656036@nku.edu.tr>
2021-01-21 14:37:55 +0300

    21 Ocak 2021 Ödev konu tekrarı

---
+++ Constructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() {Id=1,Firstname="Eray",Lastname="Demir",City="Ordu" };
+            Customer customer2 = new Customer(2, "Güllü","Demir","Ordu");
+
+
+            Console.WriteLine(customer2.Firstname);
+
+        }
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //defult constructor
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            Firstname = firstName;
+            Lastname = lastName;
+            Id = id;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler1 = new List<string>();
+            sehirler1.Add("Ordu");
+            
+           
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("İstanbul");
+            sehirler2.Add("İstanbul");
+            sehirler2.Add("İstanbul");
+            sehirler2.Add("İstanbul");
+            Console.WriteLine(sehirler2.Count);
+
+
+
+
+
+
+        }
+    }
+
+    class MyList<T>//Generic class
+    {
+        T[] array;
+        T[] temparray;
+        public MyList()
+        {
+            array = new T[0];
+        }
+        public void Add(T item)
+        {
+             temparray = array;// geçici dizi
+            array = new T[array.Length+1];
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                array[i] = temparray[i];
+            }
+            array[array.Length - 1] = item;
+        }
+
+     
+
+        public int Count
+        {
+            get { return array.Length; }
+
+        }
+
+
+    }
+
+
+
+}
